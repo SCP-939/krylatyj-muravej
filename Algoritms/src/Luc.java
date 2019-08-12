73cfd1aac2c6cea603643d8cb03c25a6492e66ac
MrFired <Milgram_S@mail.ru>
2019-08-12 11:46:26 +0300

    Bad commit

---
+++ Algoritms/src/Luc.java
@@ -0,0 +1,9 @@
+public class Luc {
+    public static String encipher(String text) {
+        return "123";
+    }
+
+    public static String decipher(String text) {
+        return null;
+    }
+}
