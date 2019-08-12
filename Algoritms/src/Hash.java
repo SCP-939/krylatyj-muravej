1ac1dde37860f2c921c43f4c5a7cf443325b3eae
SCP-939 <amirgizzatullin7777@gmail.com>
2019-08-12 13:58:54 +0300

    start

--- Algoritms/src/Hash.java
+++ Algoritms/src/Hash.java
@@ -2,7 +2,16 @@ public class Hash {
 
 
     static byte[] hash_128(String s){
+        char[] src = s.toCharArray();
         byte[] hashCode = new byte[16];
+        push(hashCode, src[0]);
         return hashCode;
+    }
+
+    static void push(byte[] b, char ch){
+        int plus = 0;
+        for (int i = 0; i < 16; i++) {
+            b[0] *= 37;
+        }
     }
 }
