7c610ed8a3457d2fe56d995ad21d8417c861bba2
CaptanP4nik <44115646+CaptanP4nik@users.noreply.github.com>
2019-08-12 15:30:20 +0300

    Add files via upload

---
+++ cod2.0.cpp
@@ -0,0 +1,42 @@
+/*#include<cmath>
+#include<map>
+#include<fstream>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<cstring>
+
+using namespace std;
+
+unsigned long long powwer(unsigned long long a, unsigned long long b, unsigned long long open_n) {
+	unsigned long long c = a;
+	for (unsigned long long i = 1; i < b; i++) {
+		c = c * a;
+		c = c % open_n;
+
+	}
+	return c % open_n;
+}
+
+int main() {
+	string open_massage;
+	getline(cin, open_massage);
+	vector<char> massage;
+	for (unsigned long long i = 0; i < open_massage.size(); i++) {
+		massage.push_back(open_massage[i]);
+	}
+	vector<unsigned long long> close_massage;
+	unsigned long long open_n = 27221;
+	unsigned long long open_e = 11;
+	for (unsigned long long i = 0; i < massage.size(); i++) {
+		unsigned long long a = unsigned long long(massage[i]);
+		a = powwer(a, open_e, open_n);
+		close_massage.push_back(a);
+	}
+	for (unsigned long long i = 0; i < massage.size(); i++) {
+		cout << close_massage[i] << '\n';
+	}
+
+
+}*/
---
+++ decod2.0.cpp
@@ -0,0 +1,37 @@
+#include<cmath>
+#include<map>
+#include<fstream>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<cstring>
+using namespace std;
+unsigned long long powwer(unsigned long long a, unsigned long long b, unsigned long long open_n) {
+	unsigned long long c = a;
+	for (unsigned long long i = 1; i < b; i++) {
+		c = c * a;c = c % open_n;
+
+	}
+	return c%open_n;
+}
+int main() {
+	string open_massage;
+	vector<unsigned long long> close_massage;
+	unsigned long long x;
+	while (cin >> x) {
+		close_massage.push_back(x);
+	}
+	unsigned long long key_p=163, key_q=167;
+	unsigned long long open_n = 27221;
+	unsigned long long f_n = 26892;
+	unsigned long long open_e=11;
+	unsigned long long kof_k=4;
+	unsigned long long key_d = (kof_k * f_n + 1) / open_e;
+	for (unsigned long long i = 0; i < close_massage.size(); i++) {
+		unsigned long long a = close_massage[i];
+		a = powwer(a, key_d, open_n);
+		open_massage.push_back(char(a));
+	}
+	cout << open_massage;
+}
